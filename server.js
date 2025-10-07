e5e32f20cc44bb044c8892a3954104ca82f89e4f
AgriConnect <technohub6911@gmail.com>
2025-10-08 05:06:45 +0800

    Remove javascript keyword from server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-javascript
+
 process.on('uncaughtException', (error) => {
   console.error('Uncaught Exception:', error);
   process.exit(1);
 });
-
 process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
   process.exit(1);
