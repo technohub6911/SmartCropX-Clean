e3c8d335a0bf69530eb8ec2d2a8e65a51dbde8ad
AgriConnect <technohub6911@gmail.com>
2025-10-08 04:46:52 +0800

    Fix import order in server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+javascript
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
+
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
